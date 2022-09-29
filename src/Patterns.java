8811b8ea80c6544d0d46139b8896f40f2081e0e9
aryall050 <armaanprabesharyall@gmail.com>
2022-09-28 18:14:02 -0600

    apple2

--- src/Patterns.java
+++ src/Patterns.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Patterns {
+public class Patterns {
+    public static void main(String[] args) {
+
+
+        for(int a=0; a<6; a++){
+
+            for(int b=0; b<=a; b++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+    }
 }
